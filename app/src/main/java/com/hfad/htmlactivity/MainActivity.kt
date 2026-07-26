a94ce9c42988f1096d973f0b34a86234b5687417
donk945 <663605896@qq.com>
2026-07-26 14:03:33 +0800

    navigation

--- app/src/main/java/com/hfad/htmlactivity/MainActivity.kt
+++ app/src/main/java/com/hfad/htmlactivity/MainActivity.kt
@@ -1,14 +1,52 @@
 package com.hfad.htmlactivity
 
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import androidx.drawerlayout.widget.DrawerLayout
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI
+import androidx.navigation.ui.onNavDestinationSelected
+import androidx.navigation.ui.setupWithNavController
+import com.google.android.material.appbar.MaterialToolbar
+import com.google.android.material.navigation.NavigationView
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val toolbar=findViewById<MaterialToolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val navHostFragment=supportFragmentManager
+            .findFragmentById(R.id.nav_host_fragment)as NavHostFragment
+        val navController=navHostFragment.navController
+
+        val navView=findViewById<NavigationView>(R.id.nav_view)
+        NavigationUI.setupWithNavController(navView,navController)
+
+        val drawer=findViewById<DrawerLayout>(R.id.drawer_layout)
+
+        val builder= AppBarConfiguration.Builder(navController.graph)
+        builder.setOpenableLayout(drawer)
+        val appBarConfiguration=builder.build()
+        toolbar.setupWithNavController(navController,appBarConfiguration)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.menu_drawer,menu)
+        return super.onCreateOptionsMenu(menu)
+    }
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        val navController = findNavController(R.id.nav_host_fragment)
+        return item.onNavDestinationSelected(navController) // 自动导航
+                || super.onOptionsItemSelected(item) // 备用处理
     }
 }
